872b3c93f25b5b74a78cf4422f698f8633878280
OlegOKovalyov <koa2003@ukr.net>
2018-09-02 16:30:21 +0300

    Second commit of Lpermission project with AdminLTE theme and new Home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <!-- Page Header -->
+        <!-- <small>Optional description</small> -->
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+            <div class="card panel panel-default">
+                <!-- <div class="card-header">Dashboard</div> -->
+
+                <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -16,8 +33,17 @@
 
                     You are logged in!
                 </div>
-            </div>
+            </div>        
+
+    </section>
+    <!-- /.content -->
+
+
+<!-- <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
         </div>
     </div>
-</div>
+</div> -->
 @endsection
